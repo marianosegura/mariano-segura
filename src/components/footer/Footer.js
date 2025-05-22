83b8431b31e0e188cba0c2d835697962d8205b45
marianosegura <luismariano10del4@gmail.com>
2025-05-22 17:46:52 -0600

    Removed project repo link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,16 +2,6 @@ import './Footer.css';
 
 const Footer = () => (
   <footer className="footer">
-    <a
-      className="footer-link"
-      href="https://github.com/marianosegura/mariano-segura"
-      target="_blank"
-      rel="noreferrer"
-    >
-      You can check{' '}
-      <span className="footer-highlight">this site repository</span> at my
-      Github
-    </a>
     <p>Made with love by Mariano Segura :)</p>
   </footer>
 );
